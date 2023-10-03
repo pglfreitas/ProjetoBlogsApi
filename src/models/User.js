ebcab696602d78cba1201441cbe59d6b3db49f75
Pedro Gabriel <pglfreitas@gmail.com>
2023-10-03 13:42:50 -0300

    Conclusão req 10

--- src/models/User.js
+++ src/models/User.js
@@ -24,6 +24,12 @@ module.exports = (sequelize, DataTypes) => {
 		tableName: 'users',
 		underscored: true,
 	  });
+	  User.associate = (models) => {
+		User.hasMany(models.BlogPost, {
+			foreignKey: 'userId',
+			as: 'BlogPost'
+		})
+	  }
   
 	return User;
   };
